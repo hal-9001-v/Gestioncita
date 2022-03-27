b125d26714730501876e14feee743f8c0375de8f
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2022-03-27 18:30:08 +0200

    Gestion Writer 1.1

--- Práctica 2/gestionWritter.py
+++ Práctica 2/gestionWritter.py
@@ -26,10 +26,29 @@ size_of_vecinos = 8
 
 size_of_casas = size_of_jugadores + size_of_vecinos
 casas_of_jugadores_start = 0
-casas_of_vecinos_start = size_de_personajes
+casas_of_vecinos_start = size_of_jugadores
 
 # This would be called casas_left but it is unresolved reference then dont make questions ¯\_(ツ)_/¯
 casas_avaliable = 30
+
+
+def turn_id(id):
+    if id >= 100:
+        return str(id)
+    if id >= 10:
+        return "0" + str(id)
+
+    return "00" + str(id)
+
+
+def remove_previous(file):
+    tree = etree.parse(file)
+    root = tree.getroot()
+
+    for child in root.getchildren():
+        root.remove(child)
+
+    tree.write(file, pretty_print=True)
 
 
 def insert_new_entry(file, tag, attributes, sub_elements):
@@ -48,7 +67,7 @@ def insert_new_entry(file, tag, attributes, sub_elements):
 def create_casas():
     casa_size = 30
     for i in range(casa_size):
-        insert_new_entry(casa_file, "casa", {"id": "c" + str(i)}, [])
+        insert_new_entry(casa_file, "casa", {"id": "c" + turn_id(i)}, [])
 
 
 def create_coleccionables():
@@ -57,13 +76,13 @@ def create_coleccionables():
     specialType = ["1", "2", "3", "4"]
     localizations = ["Cascada", "Estanque", "Mar", "Boca del rio", "Aire", "Tierra", "Flor", "Arbol", "Bajo tierra"]
     rarities = ["1", "2", "3", "4", "5"]
-    id_count = 1
+    id_count = 0
 
     for season in seasons:
         for specialType in specialType:
             for localization in localizations:
                 for rarity in rarities:
-                    attributes = {"id": "col" + str(id_count),
+                    attributes = {"id": "col" + turn_id(id_count),
                                   "tipoEsp": specialType,
                                   "estacion": season,
                                   "localizacion": localization,
@@ -77,7 +96,7 @@ def create_edificios():
     # Care to give some correlation if global variable size_edificio!
     edificio_types = ["Ayuntamiento", "Peluqueria", "Supermercado", "Museo", "Aerodromo"]
     for i in range(len(edificio_types)):
-        attributes = {"id": "ed" + str(i), "tipo": edificio_types[i]}
+        attributes = {"id": "ed" + turn_id(i), "tipo": edificio_types[i]}
         insert_new_entry(edificio_file, "edificio", attributes, [])
 
 
@@ -91,7 +110,7 @@ def create_equipables():
     for hat in hats_names:
         random_stack = random.randrange(1, 64)
         random_price = random.randrange(100, 5000)
-        attributes = {"id": "equi" + str(equip_count), "nombre": hat, "stack": str(random_stack),
+        attributes = {"id": "equi" + turn_id(equip_count), "nombre": hat, "stack": str(random_stack),
                       "precio": str(random_price),
                       "tipo": equip_types[0], "lugar_eq": equip_place[4]}
         insert_new_entry(equipable_file, "equipable", attributes, [])
@@ -101,7 +120,7 @@ def create_equipables():
     for shoes in shoes_names:
         random_stack = random.randrange(1, 64)
         random_price = random.randrange(100, 5000)
-        attributes = {"id": "equi" + str(equip_count), "nombre": shoes, "stack": str(random_stack),
+        attributes = {"id": "equi" + turn_id(equip_count), "nombre": shoes, "stack": str(random_stack),
                       "precio": str(random_price),
                       "tipo": equip_types[0], "lugar_eq": equip_place[1]}
         insert_new_entry(equipable_file, "equipable", attributes, [])
@@ -111,7 +130,7 @@ def create_equipables():
     for chest in chest_names:
         random_stack = random.randrange(1, 64)
         random_price = random.randrange(100, 5000)
-        attributes = {"id": "equi" + str(equip_count), "nombre": chest, "stack": str(random_stack),
+        attributes = {"id": "equi" + turn_id(equip_count), "nombre": chest, "stack": str(random_stack),
                       "precio": str(random_price),
                       "tipo": equip_types[0], "lugar_eq": equip_place[3]}
         insert_new_entry(equipable_file, "equipable", attributes, [])
@@ -121,7 +140,7 @@ def create_equipables():
     for tool in tools_names:
         random_stack = random.randrange(1, 64)
         random_price = random.randrange(100, 5000)
-        attributes = {"id": "equi" + str(equip_count), "nombre": tool, "stack": str(random_stack),
+        attributes = {"id": "equi" + turn_id(equip_count), "nombre": tool, "stack": str(random_stack),
                       "precio": str(random_price),
                       "tipo": equip_types[1], "lugar_eq": equip_place[0]}
         insert_new_entry(equipable_file, "equipable", attributes, [])
@@ -140,7 +159,7 @@ def create_islas():
         else:
             hemisferio = "S"
 
-        attributes = {"id": "is" + str(i), "nombre": isla_names[i], "hemisferio": hemisferio}
+        attributes = {"id": "is" + turn_id(i), "nombre": isla_names[i], "hemisferio": hemisferio}
 
         tree = etree.parse(isla_file)
         root = tree.getroot()
@@ -163,19 +182,19 @@ def create_islas():
 
         for i in range(size_edificios):
             etree.SubElement(edificio_group, "edificio",
-                             {"id": "Edificios.xml#ed" + str(i)})
+                             {"id": "Edificios.xml#ed" + turn_id(i)})
             edificios_left -= 1
             edificios_in_isla += 1
 
         personaje_group = etree.SubElement(new_isla, "personajes", )
         for i in range(size_de_personajes):
-            etree.SubElement(personaje_group, "personaje", {"id": "Personajes.xml#pj" + str(i)})
+            etree.SubElement(personaje_group, "personaje", {"id": "Personajes.xml#pj" + turn_id(i)})
 
         tree.write(isla_file, pretty_print=True)
 
 
 def create_jugadores():
-    jugadores_names = ["Pepito", "Fulanito", "Joselito", "Dio", "Vinsent"]
+    jugadores_names = ["Pepito", "Fulanito", "Joselito", "Dio", "Vinsent", "Paquito", "Julia", "Marcela", "Castania", "Milo", "Gatita", "Fidel", "Ernesto"]
 
     islas_left = size_islas
     for i in range(len(jugadores_names)):
@@ -183,9 +202,10 @@ def create_jugadores():
             islas_left = size_islas
 
         insert_new_entry(jugador_file, "jugador",
-                         {"id": "j" + str(i), "nombre": jugadores_names[i],
-                          "isla": "Islas.xml#is" + str(size_islas - islas_left),
-                          "casa": "Casas.xml#c" + str(casas_of_jugadores_start + i)}, [])
+                         {"id": "j" + turn_id(i), "nombre": jugadores_names[i],
+                          "isla": "Islas.xml#is" + turn_id(size_islas - islas_left),
+                          "casa": "Casas.xml#c" + turn_id(casas_of_jugadores_start + i)}, [])
+        islas_left -= 1
 
 
 def create_materiales():
@@ -196,7 +216,8 @@ def create_materiales():
 
     for j in range(iterations):
         for i in range(len(material_type)):
-            insert_new_entry(materiales_file, "material", {"id": "mat" + str(material_counter), "tipoEsp": material_type[i]}, [])
+            insert_new_entry(materiales_file, "material",
+                             {"id": "mat" + turn_id(material_counter), "tipoEsp": material_type[i]}, [])
             material_counter += 1
 
 
@@ -207,13 +228,23 @@ def create_muebles():
     mueble_conjuntos = ["", "Congelado", "Dorado", "Setas", "Flor de cerezo", "Bambu", "Mimbre", "Restaurante",
                         "Imperial",
                         "Flores", "Linda", "Mariana", "Veraniego", "Frutas", "Zodiaco", "Universitario"]
-    for i in range(len(muebles_names)):
-        random_stack = random.randrange(1, 64)
-        random_price = random.randrange(100, 5000)
+    adjectives = ["Mona", "Hortera", "Simple", "Elegante", "Epico", "Chuli"]
 
-        attributes = {"id": "mu" + str(i), "nombre": muebles_names[i], "stack": str(random_stack),
-                      "precio": str(random_price), "tipo": mueble_tipos[i], "conjunto": mueble_conjuntos[i]}
-        insert_new_entry(mueble_file, "mueble", attributes, [])
+    iterations = 10
+    mueble_counter = 0
+
+    for j in range(iterations):
+        for i in range(len(muebles_names)):
+            random_stack = random.randrange(1, 64)
+            random_price = random.randrange(100, 5000)
+
+            conjunto = mueble_conjuntos[random.randrange(0, len(mueble_conjuntos))]
+            mueble_name = muebles_names[i] + " " + adjectives[random.randrange(0, len(adjectives))]
+
+            attributes = {"id": "mu" + turn_id(mueble_counter), "nombre": mueble_name, "stack": str(random_stack),
+                          "precio": str(random_price), "tipo": mueble_tipos[i], "conjunto": conjunto}
+            insert_new_entry(mueble_file, "mueble", attributes, [])
+            mueble_counter += 1
 
 
 def create_personajes():
@@ -222,7 +253,7 @@ def create_personajes():
     edificios_of_personajes = ["1", "1", "2"]
     for i in range(len(personajes_names)):
         insert_new_entry(personaje_file, "personaje",
-                         {"id": "pj" + str(i), "nombre": personajes_names[i],
+                         {"id": "pj" + turn_id(i), "nombre": personajes_names[i],
                           "edificio": "Edificios.xml#ed" + edificios_of_personajes[i]},
                          [])
 
@@ -235,7 +266,7 @@ def create_props():
     for i in range(len(props_names)):
         random_stack = random.randrange(1, 64)
         random_price = random.randrange(100, 5000)
-        attributes = {"id": "prop" + str(i), "nombre": props_names[i], "stack": str(random_stack),
+        attributes = {"id": "prop" + turn_id(i), "nombre": props_names[i], "stack": str(random_stack),
                       "precio": str(random_price),
                       "tipo": props_types[i], "comestible": comestibles[i]}
         insert_new_entry(prop_file, "prop", attributes, [])
@@ -246,27 +277,40 @@ def create_vecinos():
     nombres_vecinos = ["Paco", "Joshua", "Ankha", "Marcelyn", "Apollo", "Steacy", "Carlos", "Queque"]
     personalidades = ["Alegre", "Atletico", "Esnob", "Dulce", "Grunion", "Presumido", "Perezoso", "Normal"]
     for i in range(len(nombres_vecinos)):
-        attributes = {"id": "vec" + str(i), "personalidad": personalidades[i], "nombre": nombres_vecinos[i],
-                      "casa": "Casas.xml#c" + str(casas_of_vecinos_start + i)}
+        attributes = {"id": "vec" + turn_id(i), "personalidad": personalidades[i], "nombre": nombres_vecinos[i],
+                      "casa": "Casas.xml#c" + turn_id(casas_of_vecinos_start + i)}
         insert_new_entry(vecino_file, "vecino", attributes, [])
 
 
+remove_previous(casa_file)
 create_casas()
 
+remove_previous(mueble_file)
+create_muebles()
+
+remove_previous(coleccionable_file)
 create_coleccionables()
 
+remove_previous(edificio_file)
 create_edificios()
 
+remove_previous(equipable_file)
 create_equipables()
 
+remove_previous(isla_file)
 create_islas()
 
+remove_previous(jugador_file)
 create_jugadores()
 
+remove_previous(materiales_file)
 create_materiales()
 
+remove_previous(personaje_file)
 create_personajes()
 
+remove_previous(prop_file)
 create_props()
 
+remove_previous(vecino_file)
 create_vecinos()
