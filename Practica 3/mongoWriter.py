28e226e01f6f160b3bdb24c87bbbaea53d4b9ae9
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2022-05-08 20:25:10 +0200

    Part 2

--- Practica 3/mongoWriter.py
+++ Practica 3/mongoWriter.py
@@ -7,3 +7,12 @@ file_path = os.path.dirname(__file__) + "\\Json\\"
 test_path = file_path + "text.json"
 
 
+def write_file(file, values):
+    file = open(file, "w")
+    file.write(json.dumps(values))
+    file.close()
+
+
+val = {"Jiji": "pinguino", "Caquitas": [{"Caquita 1": "Hola"},{"Caquita 2": "Hola"} ]}
+
+write_file(test_path, val)
